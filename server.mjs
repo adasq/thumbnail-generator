7daff7236f4a84694088e278713fa418934a6be5
adasq <adasq3@gmail.com>
2022-12-09 19:02:54 +0100

    Initial code upload

---
+++ server.mjs
@@ -0,0 +1,50 @@
+
+import { createThumbnail } from './thumbnail.mjs'
+import Hapi from '@hapi/hapi';
+
+;(async () => {
+
+    const server = Hapi.server({
+        port: process.env.PORT || 3000,
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: async (request, h) => {
+            return 'hi';
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/thumbnail',
+        handler: async (request, h) => {
+            console.log('request.query')
+            console.log(request.query)
+            console.log('request.payload')
+            console.log(request.payload)
+
+            const { templateUrl, ...params } = request.query;
+            console.log('params', params)
+
+            if(!templateUrl) return 'no templateUrl specified';
+
+            try {
+                 const image = await createThumbnail({
+                    templateUrl,
+                    params
+                })
+                const response = h.response(image);
+                response.type('image/jpg');
+                return response;
+            } catch(err) {
+                console.log(err);
+                return err;
+            }
+        }
+    });  
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+})()
---
+++ thumbnail.mjs
@@ -0,0 +1,29 @@
+import fetch from 'node-fetch';
+import nodeHtmlToImage from 'node-html-to-image';
+
+export const createThumbnail = async ({ templateUrl, params }, output) => {
+    const html = await fetch(templateUrl).then(a => a.text())
+
+    let template = html
+        .replace(/https:\/\/codesandbox.io/g, 'https://codesandbox2.io')
+        .replace('PARAMS = {}', `PARAMS = ${JSON.stringify(params || {})}`)
+
+    Object.entries(params).map(([key, value]) => {
+        template = template.replace(`{{${key}}}`, value);
+    });
+    
+
+    return await nodeHtmlToImage({
+        output: output || './dist/thumbnail.png',
+        quality: 100,
+        puppeteerArgs: {
+            headless: true,
+            args: [
+              '--no-sandbox',
+              '--disable-setuid-sandbox',
+            ]
+        },
+        content: {},
+        html: template
+    })
+}
